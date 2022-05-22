ecfc165837eef542eebb2b0d3636c2afcbc2b878
steveneng <steven.e11373@gmail.com>
2022-05-22 10:58:45 -0400

    added content

---
+++ pages/constants/constants.js
@@ -0,0 +1,30 @@
+export const resourceLinks = [
+    {
+      title: "Resume Builder",
+      link: "https://flowcv.io/",
+      description: "An amazing free tool to use to build resume"
+    },
+    {
+      title: "Web Development by Google",
+      link: "https://web.dev/",
+      description: "Google's guide on building stuff for the web"
+    },
+    {
+      title: "FireShip Io",
+      link: "https://fireship.io/",
+      description: "One of my favorite youtube channels. 100 second videos"
+    },
+    {
+      title: "LeetCode Problems",
+      link:
+        "https://seanprashad.com/leetcode-patterns/?fbclid=IwAR3RP9EpPEH7_ZVl0AWxkCOT2BpbAXu35jPke_A0nDs1mBc-6hFm1TxhAdk",
+      description:
+        "Most Frequently asked coding questions and labeled. If you can do them all, you are beyond prepared for the coding interview."
+    },
+    {
+      title: "Udacity Coding School",
+      link: "https://www.udacity.com/",
+      description: "Udacity has some free coding courses on various subjects"
+    }
+  ];
+  
